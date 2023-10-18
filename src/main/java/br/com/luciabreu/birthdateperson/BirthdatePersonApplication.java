a02c745789babfaffef3545ffe0032991f558b4c
Luciara-Abreu <luciara.abreu@gmail.com>
2023-10-18 10:27:04 -0300

    feat/commit initial

---
+++ src/main/java/br/com/luciabreu/birthdateperson/BirthdatePersonApplication.java
@@ -0,0 +1,13 @@
+package br.com.luciabreu.birthdateperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdatePersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdatePersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luciabreu/birthdateperson/BirthdatePersonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luciabreu.birthdateperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdatePersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
